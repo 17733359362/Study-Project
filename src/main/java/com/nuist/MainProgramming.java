65046b127300cab0b7d36e44b0428c921dfd237c
wangtianxiang <wangtianxiang@yqn.com>
2024-06-03 09:22:38 +0800

    feat:基础结构

---
+++ src/main/java/com/nuist/MainProgramming.java
@@ -0,0 +1,19 @@
+package com.nuist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @Author wtx
+ * @Date 2024/5/22
+ */
+@SpringBootApplication
+public class MainProgramming {
+    public static void main(String[] args) {
+        SpringApplication.run(MainProgramming.class, args);
+    }
+}
